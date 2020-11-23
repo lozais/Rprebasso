63c315851823970b9384475b8d0f6a9272ea2b19
checcomi <checcomi@gmail.com>
2020-11-23 15:51:01 +0000

    gv litter update

--- src/B_prebas.f90
+++ src/B_prebas.f90
@@ -37,6 +37,7 @@ implicit none
  real (kind=8) :: prelesOut(16),fAPARsite
  real (kind=8) :: leac=0 !leaching parameter for Yasso
  real (kind=8),DIMENSION(nLayers,5) :: fbAWENH,folAWENH,stAWENH
+ real (kind=8) :: gvAWENH(5)
  real (kind=8),DIMENSION(nLayers) :: Lb,Lf,Lst
 ! real (kind=8),DIMENSION(nLayers) :: speciesIDs
  real (kind=8),DIMENSION(nLayers) :: valX
@@ -555,7 +556,7 @@ if (year <= maxYearSite) then
      call fAPARgv(fAPARsite,ETSmean,siteType,GVout(year,1),GVout(year,2),sum(P0yX(:,1))/nYears) !reduced input output
      GVout(year,3) = prelesOut(1) * GVout(year,1)/fAPARsite! Photosynthesis in g C m-2 (converted to kg C m-2)
      GVout(year,4) = GVout(year,3)*0.5 !where to put those two variables
-   	 STAND_all(26,1) = STAND_all(26,1) + GVout(year,2)	!add !!!ground vegetation to the 1st layer
+   	 ! STAND_all(26,1) = STAND_all(26,1) + GVout(year,2)	!add !!!ground vegetation to the 1st layer
     elseif(fAPARsite==0.) then
 	 call fAPARgv(fAPARsite,ETSmean,siteType,GVout(year,1),GVout(year,2),sum(P0yX(:,1))/nYears) !reduced input output
      fAPARprel(:) = fAPARsite + GVout(year,1)
@@ -566,7 +567,7 @@ if (year <= maxYearSite) then
 		dailyPRELES((1+((year-1)*365)):(365*year),3), &  !daily SW
 		etmodel)		!type of ET model
       GVout(year,3) = prelesOut(1) ! Photosynthesis in g C m-2 
-      GVout(year,4) =  GVout(year,3)*0.5 !where to put those two variables
+      GVout(year,4) =  GVout(year,3)*0.5 
 	endif
    endif
 
@@ -1426,12 +1427,17 @@ modOut((year+1),9:nVar,:,:) = outt(9:nVar,:,:)
 
 !!!!run Yasso
  if(yassoRun==1.) then
-  do ijj = 1, nLayers
+
+   do ijj = 1, nLayers
    Lst(ijj) = outt(29,ijj,1)
    Lb(ijj) =  outt(28,ijj,1)
    Lf(ijj) = outt(26,ijj,1)+outt(27,ijj,1)
    species = int(initVar(1,ijj))
    call compAWENH(Lf(ijj),folAWENH(ijj,:),pAWEN(1:4,species))   !!!awen partitioning foliage
+   if(GVrun==1 .and. ijj==1) then 
+    call compAWENH(GVout(year,2),gvAWENH,pAWEN(1:4,1))   !!!awen partitioning ground vegetation for now I'm usingPine parameters
+	folAWENH(ijj,:) = folAWENH(ijj,:) + gvAWENH			 !!!add AWEN gv to 1st layer
+   endif
    call compAWENH(Lb(ijj),fbAWENH(ijj,:),pAWEN(5:8,species))   !!!awen partitioning branches
    call compAWENH(Lst(ijj),stAWENH(ijj,:),pAWEN(9:12,species))         !!!awen partitioning stems
    call mod5c(pYasso,t,weatherYasso(year,:),soilC((year),:,1,ijj),stAWENH(ijj,:),litterSize(1,species), &
