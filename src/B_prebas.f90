06165a46b1dcf95360240de129755dc0aac5385c
ForModLabUHel <francesco.minunno@helsinki.fi>
2021-04-09 13:55:43 +0100

    new mortality model

--- src/B_prebas.f90
+++ src/B_prebas.f90
@@ -6,6 +6,7 @@ subroutine prebas(nYears,nLayers,nSp,siteInfo,pCrobas,initVar,thinning,output,nT
 		fixBAinitClarcut,initCLcutRatio,ETSy,P0y,weatherPRELES,DOY,pPRELES,etmodel, soilCinOut,pYasso,pAWEN,weatherYasso,&
 		litterSize,soilCtotInOut,defaultThin,ClCut,energyCut,inDclct,&
 		inAclct,dailyPRELES,yassoRun,energyWood,tapioPars,thdPer,limPer,ftTapio,tTapio,GVout,GVrun) !energyCut
+		
 
 
 implicit none
@@ -64,6 +65,8 @@ implicit none
  real (kind=8) :: par_rhof, par_rhor, par_rhow, par_c, par_beta0, par_betab, par_betas
  real (kind=8) :: par_s1, par_p0, par_ksi, par_cr2,par_kRein,Rein, c_mort
  real (kind=8) :: BA, dA, dB, reineke(nLayers), dN, wf_test,par_thetaMax, par_H0max,par_kH, par_gamma,par_H0
+ real (kind=8) :: D13m2(nLayers),BAm2(nLayers),Nm2(nLayers),hm2(nLayers),dNm2(nLayers), dBAm2(nLayers), rfm2(nLayers)
+
  real (kind=8) :: par_rhof0, par_rhof1, par_rhof2, par_aETS,dHcCum,dHCum,pars(30), thinningType = 0.
 
 !management routines
@@ -1122,7 +1125,175 @@ endif
 
 	STAND_all(:,ij)=STAND
 end do !!!!end loop species
- end do !!!!end loop year
+ end do !!!!end loop inttimes
+
+!!!!Calculate mortality with Mikko's model
+D13m2 = STAND_all(12,:)
+BAm2 = STAND_all(13,:)/STAND_all(17,:) 
+Nm2 = STAND_all(17,:)
+hm2 = STAND_all(11,:)
+dNm2 = 0.
+dBAm2 = 0.
+rfm2 = 0.
+
+do ij = 1 , nLayers 		!loop Layer
+
+ STAND=STAND_all(:,ij)
+ species = int(stand(4))
+ param = pCrobas(:,species)
+
+ par_cR= param(1)
+ par_rhow= param(2)
+ par_sla = param(3)
+ par_k =param(4)
+ par_vf0 =param(5)
+ par_vr0 =param(6)
+ par_c=param(7)
+ par_mf0=param(8)
+ par_mr0=param(9)
+ par_mw0=param(10)
+ par_z=param(11)
+ par_beta0=param(12)
+ par_betab=param(13)
+ par_betas = param(14)
+ par_rhof2 = param(15)
+ par_s1 = param(16)
+ par_kRein = param(17)
+ par_s0scale = param(18)
+ par_x = param(19)
+ par_aETS = param(20)
+ par_alfar1 =param(21)
+ par_alfar2 =param(22)
+ par_alfar3 =param(23)
+ par_alfar4 = param(24)
+ par_alfar5 = param(25)
+ par_sarShp = param(26) !Shape surface area of the crown: 1.= cone; 2.=ellipsoide
+ par_S_branchMod = param(27) !model for branch litter model
+ p0_ref = param(29)
+ ETS_ref = param(30)
+ par_thetaMax = param(31)
+ par_H0max = param(32)
+ par_gamma = param(33)
+ par_rhof1 = 0.!param(20)
+ par_Cr2 = 0.!param(24)
+ par_kH = param(34)
+ par_sla0 = param(39)
+ par_tsla = param(40)
+
+! do siteNo = 1, nSites  !loop sites
+
+! initialize site variables
+!  sitetype = STAND(3)
+
+  age = STAND(7)
+  H = STAND(11)
+  D = STAND(12)
+  BA = STAND(13)
+  Hc = STAND(14)
+  N = BA/(pi*((D/2/100)**2))
+  B = BA/N! * par_ops2
+  A = stand(16)
+!  Cw = STAND(15)
+  Lc = H - Hc
+  hb = par_betab * Lc ** par_x
+  Cw = 2. * hb
+  STAND(15) = Cw
+  ! STAND(16) = LC !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!TO CHECK !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  ETS = STAND(5) !!##!!2
+  Light = STAND(36)
+  V = stand(30)
+  mort = stand(40)
+  par_sla = par_sla + (par_sla0 - par_sla) * Exp(-ln2 * (age / par_tsla) ** 2.)
+  
+if (N>0.) then
+
+  par_rhof0 = par_rhof1 * ETS_ref + par_rhof2
+  par_rhof = par_rhof1 * ETS + par_rhof2
+  par_vf = par_vf0 / (1. + par_aETS * (ETS-ETS_ref)/ETS_ref)
+  par_vr = par_vr0 / (1. + par_aETS * (ETS-ETS_ref)/ETS_ref) !!!new version
+
+ !calculate derived variables
+  rc = Lc / (H-1.3) !crown ratio
+  ! A = rc * B
+  wf_treeKG = par_rhof * A
+  ! par_ksi = wf_treeKG / (Lc ** par_z)
+  wf_STKG = wf_treeKG * N !needle mass per STAND in units C
+  ppow=1.6075
+
+! Mortality - use Mikko's model
+     if(time==inttimes) then
+	  call FMortality(D13m2, BAm2, Nm2, hm2, dNm2, dBAm2,rfm2, nLayers, ij)
+      dN = dNm2(ij)
+      Vold = V
+      Nold = N
+      ! if(N < 5.) N = 0.0
+
+      N = max(0.0, N + step*dN)
+
+	  if (dN<0. .and. Nold>0.) then
+			W_wsap = stand(47)
+			W_froot = stand(25)
+			W_c = stand(48) !sapwood stem below Crown
+			W_s = stand(49) !sapwood stem within crown
+			W_branch = stand(24) !branches biomass
+			W_croot = stand(32) !coarse root biomass
+			Wsh = stand(50)
+			Wdb = stand(51)
+			W_bh = stand(53)
+			W_crh = stand(54)
+			W_stem = stand(31)
+	S_branch = STAND(26) + (W_branch + W_croot*0.83 + Wdb) * min(1.,-dN*step/Nold) 
+	S_wood = STAND(29)+(W_croot*0.17 + W_stem) * min(1.,-dN*step/Nold)
+    S_fol = STAND(26) + wf_STKG * min(1.,-dN*step/Nold) !foliage litterfall
+    S_fr  = STAND(27) + W_froot * min(1.,-dN*step/Nold)  !fine root litter
+			W_wsap = W_wsap * N/Nold
+			W_froot = W_froot * N/Nold
+			W_c = W_c * N/Nold
+			W_s = W_s * N/Nold
+			W_branch = W_branch * N/Nold
+			W_croot = W_croot * N/Nold
+			Wsh = Wsh * N/Nold
+			W_bh = W_bh * N/Nold
+			W_crh = W_crh * N/Nold
+			Wdb = Wdb * N/Nold
+			W_stem = W_stem * N/Nold
+			V = V * N/Nold
+			BA = BA * N/Nold
+			wf_STKG = wf_STKG * N/Nold
+  STAND(24) = W_branch
+  STAND(25) = W_froot
+  STAND(26) = S_fol
+  STAND(27) = S_fr
+  STAND(28) = S_branch
+  STAND(29) = S_wood
+  STAND(31) = W_stem
+  STAND(32) = W_croot
+  STAND(42) = Vold - V!* min(1.,-dN*step/Nold)
+  STAND(47) = W_wsap
+  STAND(48) = W_c
+  STAND(49) = W_s
+  STAND(50) = Wsh
+  STAND(53) = W_bh
+  STAND(54) = W_crh
+  STAND(51) = Wdb
+
+      endif
+
+	  !!!calculate deadWood using Gompetz function (Makinen et al. 2006)!!!!
+	  if(dN<0.) then
+	  modOut((year+1),8,ij,1) = modOut((year+1),8,ij,1) + V* min(1.,-dN*step/N)
+	    do ijj = 1,(nyears-year)
+			modOut((year+ijj+1),8,ij,1) = modOut((year+ijj+1),8,ij,1) + (V/N) * (-dN*step) * &
+				exp(-exp(pCrobas(35,species) + pCrobas(36,species)*ijj + pCrobas(37,species)*D + 0.))
+		enddo
+	  end if
+	 endif
+endif
+  STAND_all(:,ij)=STAND
+end do !!!!!!!end loop layers
+
+!!!end mortality calcutations
+
 
 !Perform thinning or defoliation events for this time period using standard management routines!!!!!!!!!!!!!!!!
 !do siteNo = 1, nSites
